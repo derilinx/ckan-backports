2dc69bd86395095f6437f0b96a61ce9f7e902075
Eric Soroos <eric@derilinx.com>
2025-10-30 11:16:42 +0100

    update test infrastruction so that tests can run

--- test-infrastructure/install_deps.sh
+++ test-infrastructure/install_deps.sh
@@ -5,15 +5,13 @@ mkdir -p /etc/solr/conf/
 ln -s ~/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 
 # OS Dependencies
-apt update
-curl -sL https://deb.nodesource.com/setup_10.x | bash -
-apt install -y nodejs
-apt install -y libgtk2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+apt update && apt-get -y dist-upgrade
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
+apt install -y nodejs libgtk2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb postgresql-client
 npm install
-apt install -y postgresql-client
 
 #Python Dependencies
 pip install -r requirements.txt
 pip install -r dev-requirements.txt
-python setup.py develop
+pip install -e .
 pip check
